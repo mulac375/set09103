86e146f0494347f7cfa402b670b1b1f13c711945
mulac375 <mulac1000@hotmail.com>
2018-10-31 14:41:05 +0000

    git commit -m X^ ^X O

--- calum.py
+++ calum.py
@@ -1,35 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+
+posts = [
+
+    {   
+        'Band': 'Oasis',
+        'album': 'Definitely Maybe',
+        'Content': 'First post content',
+        'date_posted': 'October 28/10/2018'
+    },
+    {
+         'Band': 'Oasis',
+        'album': 'Whats the Story',
+        'Content': 'Second  post content',
+        'date_posted': 'October 28/10/2018'
+     }
+
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html'), 200
+    return render_template('home.html', posts=posts), 200
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/contact")
-def contact():
+@app.route("/contact/", methods=['GET', 'POST'])
+def account():
     return render_template('contact.html')
-
-@app.route("/albums")
+  
+@ app.route("/albums")
 def albums():
-    return render_template('albums.html')
+    return render_template('albums.html', posts=posts)
 
-@app.route('/inherits/')
-def inherits():
-    return render_template('base.htm')
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
-@app.route('/inherits/one/')
-def inherits_one():
-    return render_template('inherits1.html')
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
-@app.route('/inherits/two/')
-def inherits_two():
-    return render_template('inherits2.html')
 
 if  __name__ == '__main__':
  app.run(host='0.0.0.0', debug=True)
